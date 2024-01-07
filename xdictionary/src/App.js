71eb02760fa763427f4d17c43d2a546e31f8cfe5
Abinash <asavisingh72@gmail.com>
2024-01-07 17:49:03 +0530

    final commit

--- xdictionary/src/App.js
+++ xdictionary/src/App.js
@@ -2,24 +2,24 @@ import React, { useState, useEffect } from "react";
 import './App.css';
 
 const initialDictionary = [
- { word: "React", meaning: "A JavaScript library for building user interfaces." },
- { word: "Component", meaning: "A reusable building block in React." },
- { word: "State", meaning: "An object that stores data for a component." }
+  { word: "React", meaning: "A JavaScript library for building user interfaces." },
+  { word: "Component", meaning: "A reusable building block in React." },
+  { word: "State", meaning: "An object that stores data for a component." }
 ];
 
 function App() {
- const [searchTerm, setSearchTerm] = useState('');
- const [searchResult, setSearchResult] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResult, setSearchResult] = useState('');
 
- const handleSearch = async () => {
-      const foundWord = initialDictionary.find(entry =>
-        entry.word.toLowerCase() === searchTerm.toLowerCase()
-      );
-   setSearchResult(foundWord ? foundWord.meaning :<strong> Word not found in the dictionary.</strong>);
-    
- };
-  
- return (
+  const handleSearch = async () => {
+    const foundWord = initialDictionary.find(entry =>
+      entry.word.toLowerCase() === searchTerm.toLowerCase()
+    );
+    setSearchResult(foundWord ? foundWord.meaning : <strong> Word not found in the dictionary.</strong>);
+
+  };
+
+  return (
     <div>
       <h1>Dictionary App</h1>
       <input
@@ -31,11 +31,16 @@ function App() {
       />
       <button onClick={handleSearch}>Search</button>
       <div>
-        <p><strong>Definition:</strong></p>
-        <p><strong>{searchResult}</strong></p>
+        {searchResult &&
+          <div>
+            <p><strong>Definition:</strong></p>
+            <p><strong>{searchResult}</strong></p>
+          </div>
+        }
+
       </div>
     </div>
- );
+  );
 }
 
 export default App;
