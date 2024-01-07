5acce92f10c7761c9ae69026add92fb3ca78ce1c
Abinash <asavisingh72@gmail.com>
2024-01-07 16:32:03 +0530

    final commit

--- xdictionary/src/App.js
+++ xdictionary/src/App.js
@@ -1,44 +1,40 @@
-import './App.css';
 import React, { useState, useEffect } from "react";
+import './App.css';
 
-
+const initialDictionary = [
+ { word: "React", meaning: "A JavaScript library for building user interfaces." },
+ { word: "Component", meaning: "A reusable building block in React." },
+ { word: "State", meaning: "An object that stores data for a component." }
+];
 
 function App() {
-  const [dictionary, setDictionary] = useState([
-    { word: "React", meaning: "A JavaScript library for building user interfaces." },
-    { word: "Component", meaning: "A reusable building block in React." },
-    { word: "State", meaning: "An object that stores data for a component." }
-  ]);
+ const [searchTerm, setSearchTerm] = useState('');
+ const [isClicked, setIsClicked] = useState(false)
+ const [searchResult, setSearchResult] = useState('');
 
-  const [searchTerm, setSearchTerm] = useState('');
-  const [isClicked, setIsClicked] = useState(false)
-  const [searchResult, setSearchResult] = useState('');
-
-  
-  const handleSearch = async () => {
+ const handleSearch = async () => {
     setIsClicked(true);
     if (searchTerm !== '') {
-      const foundWord = dictionary.find(entry =>
+      const foundWord = initialDictionary.find(entry =>
         entry.word.toLowerCase() === searchTerm.toLowerCase()
       );
-  
+
       await new Promise(resolve => setTimeout(resolve, 500));
       
       setSearchResult(foundWord ? foundWord.meaning : 'Word not found in the dictionary.');
     }
-  };
+ };
 
-
-  useEffect(() => {
-    
+ useEffect(() => {
     handleSearch();
-  }, [dictionary]);
+ }, []);
   
-  return (
+ return (
     <div>
       <h1>Dictionary App</h1>
       <input
         type="text"
+        name='searchInput'
         placeholder="Search for a word..."
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
@@ -49,13 +45,7 @@ function App() {
         {isClicked && searchResult && <p>{searchResult}</p>}
       </div>
     </div>
-  );
-
-
-};
-
-
-
-
+ );
+}
 
 export default App;
